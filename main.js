d0dff2b25ef28efc41c3777d6e3c35bdc025e0ad
bealabea <beatricemarin89@yahoo.it>
2021-12-09 16:00:54 +0100

    add semicolon

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 // la classe dell'icona dell'hamburger menu
 // la classe dell'icona per chiudere il menu
 // la classe dell'hamburger menu
-const button = document.querySelector('.fa-bars')
-const closeButton = document.querySelector('.close')
+const button = document.querySelector('.fa-bars');
+const closeButton = document.querySelector('.close');
 const hamburger = document.querySelector('.hamburger-menu');
 
 // creo un evento al click dell'hamburger con una funzione
 button.addEventListener('click', function () {
-    hamburger.classList.add('active')
+    hamburger.classList.add('active');
 })
 
 // creo un evento al click dell'icona di chiusura con una funzione
