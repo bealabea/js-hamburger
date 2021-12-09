819ac030cf02094f0877b887c3fe0d77e2f6c05b
bealabea <beatricemarin89@yahoo.it>
2021-12-09 15:57:10 +0100

    js code fix

--- main.js
+++ main.js
@@ -3,15 +3,15 @@
 // la classe dell'icona per chiudere il menu
 // la classe dell'hamburger menu
 const button = document.querySelector('.fa-bars')
-const close = document.querySelector('.close')
+const closeButton = document.querySelector('.close')
 const hamburger = document.querySelector('.hamburger-menu');
 
 // creo un evento al click dell'hamburger con una funzione
-// al click il display cambia da none a block
 button.addEventListener('click', function () {
-        hamburger.style.display ="block";
+    hamburger.classList.add('active')
 })
 
-close.addEventListener('click', function () {
-    hamburger.style.display ="none";
+// creo un evento al click dell'icona di chiusura con una funzione
+closeButton.addEventListener('click', function () {
+    hamburger.classList.remove('active');
 })
